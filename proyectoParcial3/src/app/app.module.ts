ec91e1b21f3afe2aca17347afb6ecf4be2f8951d
Aaron <daaroncg135@gmail.com>
2023-11-27 18:02:37 -0600

    Solucion commits

--- proyectoParcial3/src/app/app.module.ts
+++ proyectoParcial3/src/app/app.module.ts
@@ -3,20 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegistrarComponent } from './components/registrar/registrar.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
-
     NavbarComponent,
     HomeComponent,
     RegistrarComponent
-
   ],
   imports: [
     BrowserModule,
