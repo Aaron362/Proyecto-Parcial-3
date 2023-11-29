31dab2685abad2d35b4b5ecd6d8f5a25c01565f4
Aaron <daaroncg135@gmail.com>
2023-11-29 14:07:44 -0600

    Configuracion Auth0 DashBoard

--- proyectoParcial3/src/app/app.component.ts
+++ proyectoParcial3/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyectoParcial3';
+//  constructor(private auth: AuthService)
 }
--- proyectoParcial3/src/app/services/auth.service.ts
+++ proyectoParcial3/src/app/services/auth.service.ts
@@ -4,6 +4,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  // import { AuthModule } from '@auth0/auth0-angular';
+
+  // @NgModule({
+  //   declarations: [AppComponent],
+  //   imports: [
+  //     BrowserModule,
+
+  //     // Import the module into the application, with configuration
+  //     AuthModule.forRoot({
+  //       domain: 'dev-dzui3uzavz6k2f80.us.auth0.com',
+  //       clientId: 'JBcKYxM55lqbfufjCpl4H8CLxO4HLoTv',
+  //       authorizationParams: {
+  //         redirect_uri: window.location.origin
+  //       }
+  //     }),
+  //   ],
 
   constructor() { }
 }
